375a51a7ccc27289b2a98e4655a9b0bc10d73ad6
22llll <116804671+22llll@users.noreply.github.com>
2022-10-27 19:50:16 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -15,15 +15,15 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小陶先生',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'omdys6dQ2GRHEIesjbNIkFrEz_mg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '9ObB2RKXYhbc7xPjqJfr-_Ep-NMRJpwPPpd2Q75jQ1g	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-25',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2001', date: '09-025',
+          type: '*生日', name: '小陶', year: '2001', date: '09-025',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
